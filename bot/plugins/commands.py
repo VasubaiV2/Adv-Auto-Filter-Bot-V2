fa214fafce79c2b1f6957b7798d420de8425d126
ZakirbhaiBott <87867737+ZakirbhaiBott@users.noreply.github.com>
2021-08-14 07:16:34 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -35,7 +35,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    '⭕ Main Channel ⭕', url="https://t.me/malayalam_movies_h"
                                 )
                         ]
                     ]
