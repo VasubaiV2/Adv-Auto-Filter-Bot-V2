a96b71c9077aa3a36e432c6dbd1687d7163f1856
VasubaiV2 <90544461+VasubaiV2@users.noreply.github.com>
2021-09-13 09:23:08 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -11,6 +11,7 @@ db = Database()
 
 
 @Client.on_message(filters.command(["start"]) & filters.private.group=1)
+
 async def start(bot, update):
 
 
@@ -18,7 +19,7 @@ async def start(bot, update):
   if update_channel:
      try:
         user = await bot.get_chat_member(update_channel, update.chat.id)
-        if user.status == "kicked":
+        if user.status =="kicked":
             await update.reply_text("sorry dude, you are **banned 🚫🚫**")
             return
         except UserNotParticipant:
